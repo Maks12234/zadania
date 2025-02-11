5482256dc47e9a2adc3e1b19978aa686be570827
Maks12234 <maksymilian.mania@zseiulaziska.pl>
2025-02-11 11:44:57 +0100

    zadania

---
+++ zadanie1.js
@@ -0,0 +1,6 @@
+function funkcja() {
+    let wartosc1 = document.getElementById("wartosc1").value;
+    let wartosc2 = document.getElementById("wartosc2").value;
+    let sum = Number(wartosc1) + Number(wartosc2);
+    document.getElementById("+").value = sum;
+}
